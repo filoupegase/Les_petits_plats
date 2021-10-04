5e00de18e2badd5f925c75e130f0a3294f4a5aae
filoupegase <loisoncorentin@yahoo.fr>
2021-10-04 20:13:41 +0200

    front done/card innerhtml

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // import { recipes } from "../data/recipes";
-
+const cardsInner = document.getElementById('cards-wrapper');
 const JsonUrl = "../data/recipes.json";
 
 // fetch(JsonUrl)
@@ -19,6 +19,29 @@ async function loadRecipes() {
 
   recipes.forEach(recipe => {
     console.log(recipe);
+
+    cardsInner.innerHTML += `<article class="c-card lg4 md6 sm12" data-card-id="${
+      recipe.id
+    }">
+        <div class="c-card__img"></div>
+        <div class="c-card__body">
+          <h2 class="c-card__title">
+            <span class="name">${recipe.name}</span>
+            <span class="duration">
+              <i class="far fa-clock"></i>
+              ${recipe.time} min
+            </span>
+          </h2>
+          <div class="c-card__recipe row-12 has-gutter-lg">
+            <div class="c-card__ingredients lg6 md6 sm6">
+              ...ingredient
+            </div>
+            <div class="c-card__description lg6 md6 sm6">
+              <p>${recipe.description}</p>
+            </div>
+          </div>
+        </div>
+      </article>`
   })
 }
 
