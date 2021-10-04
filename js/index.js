348d7b9d4ba3fbaa9b1d67d66e1712e120c43f7d
filoupegase <loisoncorentin@yahoo.fr>
2021-10-04 19:47:59 +0200

    load recipes data fetcher

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-import { recipes } from "../data/recipes";
+// import { recipes } from "../data/recipes";
 
-console.log(recipes);
+const JsonUrl = "../data/recipes.json";
+
+// fetch(JsonUrl)
+//   .then(response => {
+//     return console.log(response.json)
+//   }).then()
+//   .catch(error => {
+//   console.error(error);
+// })
+
+async function loadRecipes() {
+  const response = await fetch(JsonUrl, {
+    method: 'GET'
+  });
+  const recipes = await response.json();
+  console.log(recipes);
+
+  recipes.forEach(recipe => {
+    console.log(recipe);
+  })
+}
+
+loadRecipes();
