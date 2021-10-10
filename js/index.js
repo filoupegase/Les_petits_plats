b4b9d5be141d09bf4a8e29cc32fd8b385257e546
filoupegase <loisoncorentin@yahoo.fr>
2021-10-10 17:13:49 +0200

    test algo mesure

--- js/index.js
+++ js/index.js
@@ -35,5 +35,45 @@ function makeHashTableForSearchingRecipes(recipesList) {
 const HASH_TABLE_FOR_SEARCHING_RECIPES =
   makeHashTableForSearchingRecipes(recipesList);
 
-
 console.log(HASH_TABLE_FOR_SEARCHING_RECIPES);
+
+measureAlgorithmPerformance();
+
+function measureAlgorithmPerformance() {
+  const TEST_REQUEST_1 = {
+    userInput: "coco",
+    joinBadges: "sucre beurre"
+  };
+
+  const TEST_REQUEST_2 = {
+    userInput: "ananas",
+    joinBadges: "maîs basilic"
+  };
+
+  const TESTS_QUANTITY =1_000;
+
+  let TEST_STARTING = Date.now();
+  // console.log(TEST_STARTING)
+
+  let searchTestResult1, searchTestResult2;
+
+  for (let _ = 0; _ < TESTS_QUANTITY; _++) {
+    searchTestResult1 = recipesList.search(
+      TEST_REQUEST_1,
+      HASH_TABLE_FOR_SEARCHING_RECIPES
+    );
+    searchTestResult2 = recipesList.search(
+      TEST_REQUEST_2,
+      HASH_TABLE_FOR_SEARCHING_RECIPES
+    );
+  }
+
+  let TEST_ENDING = Date.now();
+  // console.log(TEST_ENDING)
+
+  const DURATION = TEST_ENDING - TEST_STARTING;
+
+  console.log(
+    `${2 * TESTS_QUANTITY} recherches réalisées en ${DURATION} ms`
+  );
+}
