07486eb89956ee220aeea5d43c670d04c4404792
cloison <corentin.loison@ls-vr.com>
2023-05-03 18:22:20 +0200

    fix-indent my old js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
 import { RECIPES } from "./data/recipesData.js";
-import { DataFetcher,
-  buildHashTableForSearchingRecipes,
-} from "./data/dataFetcher.js";
+import { DataFetcher, buildHashTableForSearchingRecipes } from "./data/dataFetcher.js";
 import { HomePageBuilder } from "./pages/homePageBuilder.js";
+
 
 const dataFetcher = new DataFetcher(RECIPES);
 const recipesList = dataFetcher.getRecipesList();
-const HASH_TABLE_FOR_SEARCHING_RECIPES =
-  buildHashTableForSearchingRecipes(recipesList);
-
+const HASH_TABLE_FOR_SEARCHING_RECIPES = buildHashTableForSearchingRecipes(recipesList);
 
 new HomePageBuilder(recipesList, HASH_TABLE_FOR_SEARCHING_RECIPES).render();
