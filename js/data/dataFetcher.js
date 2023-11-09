360b632bc61fe9271282ddd08dc601d9da9f907d
filoupegase <loisoncorentin@yahoo.fr>
2023-11-09 10:26:21 +0100

    update js files

--- js/data/dataFetcher.js
+++ js/data/dataFetcher.js
@@ -1,6 +1,7 @@
 import { removeStopWords } from "../utils/utilitis.js";
 import { Recipe, RecipesList } from "./recipe.js";
 
+
 /**
  * 1/
  * @constructor
@@ -8,70 +9,70 @@ import { Recipe, RecipesList } from "./recipe.js";
  * @return {recipes: Array.Object} RecipesList
  */
 export class DataFetcher {
-  constructor(dataSource) {
-    this._dataSource = dataSource;
-  }
-
-  getRecipesList() {
-    const recipes = [];
-
-    for (let recipe of this._dataSource) {
-      recipes.push(
-        new Recipe(
-          recipe.id,
-          recipe.name,
-          recipe.cover,
-          recipe.altText,
-          recipe.servings,
-          recipe.ingredients,
-          recipe.time,
-          recipe.description,
-          recipe.appliance,
-          recipe.ustensils
-        )
-      );
+    constructor(dataSource) {
+        this._dataSource = dataSource;
     }
-    // console.log(new RecipesList(recipes));
-    return new RecipesList(recipes);
-  }
+
+    getRecipesList() {
+        const recipes = [];
+
+        for (let recipe of this._dataSource) {
+            recipes.push(
+                new Recipe(
+                    recipe.id,
+                    recipe.name,
+                    recipe.cover,
+                    recipe.altText,
+                    recipe.servings,
+                    recipe.ingredients,
+                    recipe.time,
+                    recipe.description,
+                    recipe.appliance,
+                    recipe.ustensils
+                )
+            );
+        }
+        // console.log(new RecipesList(recipes));
+        return new RecipesList(recipes);
+    }
 }
 
 function extractKeywordsFromRecipe(recipe) {
-  let recipeWords = `${recipe.nameWithoutAccent} ${recipe.joinedIngredientsWithoutAccent} ${recipe.applianceNameWithoutAccent} ${recipe.joinedUstensilsWithoutAccent} ${recipe.descriptionWithoutAccent}`;
+    let recipeWords = `${ recipe.nameWithoutAccent } ${ recipe.joinedIngredientsWithoutAccent } ${ recipe.applianceNameWithoutAccent } ${ recipe.joinedUstensilsWithoutAccent } ${ recipe.descriptionWithoutAccent }`;
 
-  recipeWords = recipeWords.split(" ");
+    recipeWords = recipeWords.split(" ");
 
-  return removeStopWords(recipeWords);
+    return removeStopWords(recipeWords);
 }
 
 function addRecipeKeywordsToHashTable(recipe, recipeKeywords, hashTable) {
-  for (let keyword of recipeKeywords) {
-    for (let i = 1; i <= keyword.length; i++) {
-      const truncatedKeyword = keyword.slice(0, i);
+    for (let keyword of recipeKeywords) {
+        for (let i = 1; i <= keyword.length; i++) {
+            const truncatedKeyword = keyword.slice(0, i);
 
-      if (truncatedKeyword in hashTable) {
-        hashTable[truncatedKeyword].add(recipe);
-      } else {
-        hashTable[truncatedKeyword] = new Set([recipe]);
-      }
+            if (truncatedKeyword in hashTable) {
+                hashTable[truncatedKeyword].add(recipe);
+            } else {
+                hashTable[truncatedKeyword] = new Set([recipe]);
+            }
+        }
     }
-  }
 
-  return hashTable;
+    return hashTable;
 }
 
 export function buildHashTableForSearchingRecipes(recipesList) {
-  let hashTableForSearchingRecipes = {};
+    let hashTableForSearchingRecipes = {};
 
-  for (let recipe of recipesList.recipes) {
-    const recipeKeywords = extractKeywordsFromRecipe(recipe);
+    for (let recipe of recipesList.recipes) {
+        const recipeKeywords = extractKeywordsFromRecipe(recipe);
 
-    hashTableForSearchingRecipes = addRecipeKeywordsToHashTable(
-      recipe,
-      recipeKeywords,
-      hashTableForSearchingRecipes
-    );
-  }
+        hashTableForSearchingRecipes = addRecipeKeywordsToHashTable(
+            recipe,
+            recipeKeywords,
+            hashTableForSearchingRecipes
+        );
+    }
 
-  return hashTableForSearchingRecipes;
+    return hashTableForSearchingRecipes;
 }
--- js/data/recipe.js
+++ js/data/recipe.js
@@ -1,168 +1,171 @@
 import {
-  capitalizeFirstChar,
-  keepOnlyLettersAndRemoveAccents,
-  removeStopWords,
-  sortAlphabetically,
+    capitalizeFirstChar,
+    keepOnlyLettersAndRemoveAccents,
+    removeStopWords,
+    sortAlphabetically,
 } from "../utils/utilitis.js";
 
+
 export class Recipe {
-  /**
-   * propsValidation
-   * @constructs
-   * @param {number} id
-   * @param {string} name
-   * @param {number} servings
-   * @param {Array.Object} ingredients
-   * @param {number} time
-   * @param {string} description
-   * @param {string} appliance
-   * @param {Array.string} ustensils
-   */
-  constructor(
-    id,
-    name,
-    cover,
-    altText,
-    servings,
-    ingredients,
-    time,
-    description,
-    appliance,
-    ustensils
-  ) {
-    this.id = id;
-    this.name = name;
-    this.cover = cover;
-    this.altText = altText;
-    this.servings = servings;
-    this.ingredients = ingredients;
-    this.time = time;
-    this.description = description;
-    this.appliance = appliance;
-    this.ustensils = ustensils;
-  }
-
-  get applianceNameWithoutAccent() {
-    return keepOnlyLettersAndRemoveAccents(this.appliance);
-  }
-
-  get descriptionWithoutAccent() {
-    return keepOnlyLettersAndRemoveAccents(this.description);
-  }
-
-  get joinedIngredientsWithoutAccent() {
-    const ingredientsList = [];
-
-    for (let item of this.ingredients) {
-      ingredientsList.push(keepOnlyLettersAndRemoveAccents(item.ingredient));
+    /**
+     * propsValidation
+     * @constructs
+     * @param {number} id
+     * @param {string} name
+     * @param cover
+     * @param altText
+     * @param {number} servings
+     * @param {Array.Object} ingredients
+     * @param {number} time
+     * @param {string} description
+     * @param {string} appliance
+     * @param {Array.string} ustensils
+     */
+    constructor(
+        id,
+        name,
+        cover,
+        altText,
+        servings,
+        ingredients,
+        time,
+        description,
+        appliance,
+        ustensils
+    ) {
+        this.id = id;
+        this.name = name;
+        this.cover = cover;
+        this.altText = altText;
+        this.servings = servings;
+        this.ingredients = ingredients;
+        this.time = time;
+        this.description = description;
+        this.appliance = appliance;
+        this.ustensils = ustensils;
     }
 
-    return ingredientsList.join(" ");
-  }
-
-  get nameWithoutAccent() {
-    return keepOnlyLettersAndRemoveAccents(this.name);
-  }
-
-  get joinedUstensilsWithoutAccent() {
-    const ustensilsList = [];
-
-    for (let ustensil of this.ustensils) {
-      ustensilsList.push(keepOnlyLettersAndRemoveAccents(ustensil));
+    get applianceNameWithoutAccent() {
+        return keepOnlyLettersAndRemoveAccents(this.appliance);
     }
 
-    return ustensilsList.join(" ");
-  }
+    get descriptionWithoutAccent() {
+        return keepOnlyLettersAndRemoveAccents(this.description);
+    }
+
+    get joinedIngredientsWithoutAccent() {
+        const ingredientsList = [];
+
+        for (let item of this.ingredients) {
+            ingredientsList.push(keepOnlyLettersAndRemoveAccents(item.ingredient));
+        }
+
+        return ingredientsList.join(" ");
+    }
+
+    get nameWithoutAccent() {
+        return keepOnlyLettersAndRemoveAccents(this.name);
+    }
+
+    get joinedUstensilsWithoutAccent() {
+        const ustensilsList = [];
+
+        for (let ustensil of this.ustensils) {
+            ustensilsList.push(keepOnlyLettersAndRemoveAccents(ustensil));
+        }
+
+        return ustensilsList.join(" ");
+    }
 }
 
 export class RecipesList {
-  constructor(recipes) {
-    this.recipes = recipes;
-    this._sortByName();
-  }
-
-  get sortedAppliances() {
-    return sortAlphabetically(this._collectAppliances());
-  }
-
-  get sortedIngredients() {
-    return sortAlphabetically(this._collectIngredients());
-  }
-
-  get sortedUstensils() {
-    return sortAlphabetically(this._collectUstensils());
-  }
-
-  _sortByName() {
-    return this.recipes.sort((r1, r2) => {
-      const name1 = r1.name.toLowerCase();
-      const name2 = r2.name.toLowerCase();
-
-      const [sortedName1, sortedName2] = sortAlphabetically([name1, name2]);
-
-      if (sortedName1 === name2) return 1;
-      if (sortedName1 === name1) return -1;
-      return 0;
-    });
-  }
-
-  _collectAppliances() {
-    const appliances = new Set();
-
-    for (let recipe of this.recipes) {
-      appliances.add(capitalizeFirstChar(recipe.appliance));
+    constructor(recipes) {
+        this.recipes = recipes;
+        this._sortByName();
     }
 
-    return [...appliances];
-  }
-
-  _collectIngredients() {
-    const ingredients = new Set();
-
-    for (let recipe of this.recipes) {
-      for (let item of recipe.ingredients) {
-        ingredients.add(capitalizeFirstChar(item.ingredient));
-      }
+    get sortedAppliances() {
+        return sortAlphabetically(this._collectAppliances());
     }
 
-    return [...ingredients];
-  }
-
-  _collectUstensils() {
-    const ustensils = new Set();
-
-    for (let recipe of this.recipes) {
-      for (let ustensil of recipe.ustensils) {
-        ustensils.add(capitalizeFirstChar(ustensil));
-      }
+    get sortedIngredients() {
+        return sortAlphabetically(this._collectIngredients());
     }
 
-    return [...ustensils];
-  }
-
-  search(userRequest, hashTableForSearchingRecipes) {
-    userRequest = `${userRequest.userInput} ${userRequest.joinedBadges}`;
-
-    const words = userRequest.split(" ");
-    const keywords = removeStopWords(words);
-
-    let filteredRecipes = new Set(this.recipes);
-
-    for (let keyword of keywords) {
-      // get all recipes containing this keyword :
-      keyword = keepOnlyLettersAndRemoveAccents(keyword);
-
-      const keywordRecipes =
-        keyword in hashTableForSearchingRecipes
-          ? hashTableForSearchingRecipes[keyword]
-          : new Set();
-
-      // intersect keywordRecipes with actual filteredRecipes :
-      filteredRecipes = new Set(
-        [...keywordRecipes].filter((recipe) => filteredRecipes.has(recipe))
-      );
+    get sortedUstensils() {
+        return sortAlphabetically(this._collectUstensils());
     }
 
-    return new RecipesList([...filteredRecipes]);
-  }
+    _sortByName() {
+        return this.recipes.sort((r1, r2) => {
+            const name1 = r1.name.toLowerCase();
+            const name2 = r2.name.toLowerCase();
+
+            const [sortedName1, sortedName2] = sortAlphabetically([name1, name2]);
+
+            if (sortedName1 === name2) return 1;
+            if (sortedName1 === name1) return -1;
+            return 0;
+        });
+    }
+
+    _collectAppliances() {
+        const appliances = new Set();
+
+        for (let recipe of this.recipes) {
+            appliances.add(capitalizeFirstChar(recipe.appliance));
+        }
+
+        return [...appliances];
+    }
+
+    _collectIngredients() {
+        const ingredients = new Set();
+
+        for (let recipe of this.recipes) {
+            for (let item of recipe.ingredients) {
+                ingredients.add(capitalizeFirstChar(item.ingredient));
+            }
+        }
+
+        return [...ingredients];
+    }
+
+    _collectUstensils() {
+        const ustensils = new Set();
+
+        for (let recipe of this.recipes) {
+            for (let ustensil of recipe.ustensils) {
+                ustensils.add(capitalizeFirstChar(ustensil));
+            }
+        }
+
+        return [...ustensils];
+    }
+
+    search(userRequest, hashTableForSearchingRecipes) {
+        userRequest = `${ userRequest.userInput } ${ userRequest.joinedBadges }`;
+
+        const words = userRequest.split(" ");
+        const keywords = removeStopWords(words);
+
+        let filteredRecipes = new Set(this.recipes);
+
+        for (let keyword of keywords) {
+            // get all recipes containing this keyword :
+            keyword = keepOnlyLettersAndRemoveAccents(keyword);
+
+            const keywordRecipes =
+                keyword in hashTableForSearchingRecipes
+                    ? hashTableForSearchingRecipes[keyword]
+                    : new Set();
+
+            // intersect keywordRecipes with actual filteredRecipes :
+            filteredRecipes = new Set(
+                [...keywordRecipes].filter((recipe) => filteredRecipes.has(recipe))
+            );
+        }
+
+        return new RecipesList([...filteredRecipes]);
+    }
 }
